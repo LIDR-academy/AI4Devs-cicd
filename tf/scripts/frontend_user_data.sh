1e1977d89502e57f3e84a094025a145c2099b7c3
Pedro Alejandro Avila <alejox31@gmail.com>
2025-05-19 22:53:06 -0400

    adding stuff

--- tf/scripts/frontend_user_data.sh
+++ tf/scripts/frontend_user_data.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-yum update -y
-yum install -y docker
+sudo yum update -y
+sudo yum install -y docker
 
 # Iniciar el servicio de Docker
-service docker start
+sudo service docker start
 
 # Descargar y descomprimir el archivo frontend.zip desde S3
-aws s3 cp s3://lti-project-code-bucket/frontend.zip /home/ec2-user/frontend.zip
+aws s3 cp s3://ai4devs-project-code-bucket/frontend.zip /home/ec2-user/frontend.zip
 unzip /home/ec2-user/frontend.zip -d /home/ec2-user/
 
 # Construir la imagen Docker para el frontend
 cd /home/ec2-user/frontend
-docker build -t lti-frontend .
+sudo docker build -t lti-frontend .
 
 # Ejecutar el contenedor Docker
-docker run -d -p 3000:3000 lti-frontend
+sudo docker run -d -p 3000:3000 lti-frontend
 
 # Timestamp to force update
 echo "Timestamp: ${timestamp}"
